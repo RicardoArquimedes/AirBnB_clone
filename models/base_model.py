3d6675c730ae3102a397f5319dc3edd7dc33dc36
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2020-11-03 16:55:05 +0000

    Task 4 with kwargs and args

--- models/base_model.py
+++ models/base_model.py
@@ -17,21 +17,20 @@ class BaseModel:
         """
         constructor
         """
-        for key, value in kwargs.items():
-            if key == ' created_at':
-                    self.created_at = datetime.strptime(kwargs.get(key),
-                                                        "%Y-%m-%dT%H:%M:%S.%f")
-            elif key == 'updated_at':
-                self.updated_at = datetime.strptime(
-                    kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
-            elif key == "__class__":
-                pass
-            elif key is not "__class__":
-                self.__dict__[key] = value
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.save()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at':
+                    self.created_at = datetime.strptime(
+                                                    value,
+                                                    "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == 'updated_at':
+                    self.updated_at = datetime.strptime(
+                                                    value,
+                                                    "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == '__class__':
+                    pass
+                else:
+                    setattr(self, key, value)
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.save()
