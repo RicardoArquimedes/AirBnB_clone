d2b05c3a1a7dd2b0f293af2c035dac39a401eaea
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2020-11-03 15:55:37 +0000

    Updating

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,8 @@ class BaseModel:
     attributes/methods for other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        if kwargs 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.save()
@@ -26,8 +27,7 @@ class BaseModel:
         """
         Updates the public instance attribute updated_at
         """
-        self.updated_at = datetime.now()
-        return self.updated_at
+        return self.updated_at = datetime.now()
 
     def to_dict(self):
         """
